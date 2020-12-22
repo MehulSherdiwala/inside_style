c94897bea0def57a5e973a1587bd9ae56d6641b8
Manthan <msherdiwala16@gmail.com>
2020-12-22 15:35:18 +0530

    a

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,5 @@
 /*-----------------------------------------------------------------------------------
 
-    Theme Name: Archo
-    Theme URI: http://
-    Description: Architecture & Interior Design HTML Template
-    Author: UI-ThemeZ
-    Author URI: http://themeforest.net/user/UI-ThemeZ
-    Version: 1.0
 
 -----------------------------------------------------------------------------------*/
 
